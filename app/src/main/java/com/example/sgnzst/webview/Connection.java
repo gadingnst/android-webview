01648eda68b26a6652ca76d68a4a2809ef658235
Sutan Nst <sutan.gnst@gmail.com>
2018-12-22 03:21:13 +0700

    refactor connection

--- app/src/main/java/com/example/sgnzst/webview/Connection.java
+++ app/src/main/java/com/example/sgnzst/webview/Connection.java
@@ -5,14 +5,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class Connection {
-
+    private static final String TAG = Connection.class.getSimpleName();
     public static boolean hasConnectivity(Context context){
-        NetworkInfo info = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
         if (info == null){
             return false;
         }else{
             return true;
         }
     }
-
 }
